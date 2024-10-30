5c5c942f3e267a2a225ed8c76206fc4eefd3f73e
Lan Sovinc <lan.sovinc@gmail.com>
2024-10-31 00:59:21 +0100

    Initial commit

---
+++ src/theme/theme.ts
@@ -0,0 +1,16 @@
+'use client'
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#664DFF',
+    },
+    secondary: {
+      main: '#F7931A',
+    },
+  },
+})
+
+export default theme
