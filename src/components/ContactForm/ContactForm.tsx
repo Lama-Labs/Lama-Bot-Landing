c12b6fafeec4c48dba33fd3c1a2c9cd0d0cbe319
Matej Horvat <matej@horvat.si>
2024-11-16 12:48:45 +0100

    Add push bullet to contact form.

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,19 +1,84 @@
+'use client'
+
+import { LoadingButton } from '@mui/lab'
 import {
+  Alert,
   Box,
-  Button,
   Card,
   Container,
   TextField,
   Typography,
 } from '@mui/material'
+import Snackbar, { SnackbarCloseReason } from '@mui/material/Snackbar'
 import { Send } from 'lucide-react'
 import { useTranslations } from 'next-intl'
+import { useState } from 'react'
+
+import { sendToPushBullet } from '@/app/api/PushBulletSend'
 
 const ContactForm = () => {
   const t = useTranslations('home.cta')
 
+  const [email, setEmail] = useState<string>('')
+  const [isValidEmail, setIsValidEmail] = useState<boolean>(false)
+  const [snackbarOpen, setSnackbarOpen] = useState<boolean>(false)
+  const [sending, setSending] = useState<boolean>(false)
+
+
+  const validateEmail = (value: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return emailRegex.test(value)
+  }
+
+  // Handler for TextField input change
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value
+    setEmail(value)
+    setIsValidEmail(validateEmail(value)) // Update validity based on email format
+  }
+
+  // Show an alert with the email when the button is clicked
+  const handleClick = async () => {
+    try {
+      setSending(true);
+      const resp = await sendToPushBullet("email", email);
+      setEmail("");
+      console.log("Response:", resp);
+    } catch (error) {
+      console.error("An error occurred while sending to PushBullet:", error);
+      setSnackbarOpen(true);
+    }
+    finally {
+      setSending(false);
+    }
+  }
+
+  const handleClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: SnackbarCloseReason,
+  ) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setSnackbarOpen(false);
+  };
+
   return (
     <Container maxWidth='xl'>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={4000}
+        onClose={handleClose}
+      >
+        <Alert
+          onClose={handleClose}
+          severity="error"
+          sx={{ width: '100%' }}
+        >
+          An error occurred while sending the email.
+        </Alert>
+      </Snackbar>
       <Box
         sx={{
           display: 'flex',
@@ -59,10 +124,21 @@ const ContactForm = () => {
                 variant='outlined'
                 fullWidth
                 size='small'
+                value={email}
+                onChange={handleChange}
+                error={!isValidEmail && email.length > 0}
+                helperText={!isValidEmail && email.length > 0 ? "Please enter a valid email" : ""}
               />
-              <Button type='submit' variant='contained' color='primary'>
+              <LoadingButton
+                type='submit'
+                variant='contained'
+                color='primary'
+                loading={sending}
+                disabled={!isValidEmail}
+                onClick={handleClick}
+              >
                 <Send />
-              </Button>
+              </LoadingButton >
             </Box>
           </Box>
         </Card>
