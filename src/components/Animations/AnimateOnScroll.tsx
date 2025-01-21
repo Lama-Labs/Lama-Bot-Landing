e7dcf5f71dbe7bb92fc14cc3d38ecb0db4a15373
Lan Sovinc <lan.sovinc@gmail.com>
2025-01-21 23:21:55 +0100

    possible animation fix

--- src/components/Animations/AnimateOnScroll.tsx
+++ src/components/Animations/AnimateOnScroll.tsx
@@ -6,22 +6,25 @@ const AnimateOnScroll: React.FC = () => {
   const animateOnScroll = () => {
     const elements = document.querySelectorAll('.animate')
 
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        const windowHeight =
-          window.innerHeight || document.documentElement.clientHeight
-        const visibleHeight =
-          Math.min(entry.boundingClientRect.bottom, windowHeight) -
-          Math.max(entry.boundingClientRect.top, 0)
-        const visiblePercentage =
-          (visibleHeight / entry.boundingClientRect.height) * 100
-        if (entry.isIntersecting && visiblePercentage >= 0.05) {
-          entry.target.classList.add('in-view')
-        } else if (entry.boundingClientRect.top > window.innerHeight) {
-          entry.target.classList.remove('in-view')
-        }
-      })
-    })
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const windowHeight =
+            window.innerHeight || document.documentElement.clientHeight
+          const visibleHeight =
+            Math.min(entry.boundingClientRect.bottom, windowHeight) -
+            Math.max(entry.boundingClientRect.top, 0)
+          const visiblePercentage =
+            (visibleHeight / entry.boundingClientRect.height) * 100
+          if (entry.isIntersecting && visiblePercentage >= 0.05) {
+            entry.target.classList.add('in-view')
+          } else if (entry.boundingClientRect.top > window.innerHeight) {
+            entry.target.classList.remove('in-view')
+          }
+        })
+      },
+      { threshold: [0, 0.05, 1.0] }
+    )
 
     elements.forEach((element) => {
       observer.observe(element)
